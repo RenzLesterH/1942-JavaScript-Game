22be164128a098c4e9b5f6318a729f58be5ad750
Renz Lester J. Halagao <halagao72@gmail.com>
2022-10-11 10:41:22 +0800

    script.js codes has been converted intp OOP and ES6 in file script_OOP

--- script.js
+++ script.js
@@ -11,145 +11,145 @@ window.onload = function() {
     const enemies2 = [{x:300,y:100}, {x:380, y:20}, {x:100,y:50}];
     let bullets  = [];
 
-    function displayHero() {
-        document.getElementById('hero').style['top'] = hero.y + "px";
-        document.getElementById('hero').style['left'] = hero.x + "px";
-    }
+    // function displayHero() {
+    //     document.getElementById('hero').style['top'] = hero.y + "px";
+    //     document.getElementById('hero').style['left'] = hero.x + "px";
+    // }
 
-    function displayEnemies(){
-        let output = '';
-        for (let i = 0; i < enemies.length; i++) {
-            output += "<div class='enemy1' style='top:"+enemies[i].y+"px; left:"+enemies[i].x+"px;'></div>";
-        }
-        document.getElementById('enemies').innerHTML = output; 
-    }
+    // function displayEnemies(){
+    //     let output = '';
+    //     for (let i = 0; i < enemies.length; i++) {
+    //         output += "<div class='enemy1' style='top:"+enemies[i].y+"px; left:"+enemies[i].x+"px;'></div>";
+    //     }
+    //     document.getElementById('enemies').innerHTML = output; 
+    // }
 
-    function displayEnemies2(){
-        let output = '';
-        for (let i = 0; i < enemies2.length; i++) {
-            output += "<div class='enemy2' style='top:"+enemies2[i].y+"px; left:"+enemies2[i].x+"px;'></div>";
-        }
-        document.getElementById('enemies2').innerHTML = output; 
-    }
+    // function displayEnemies2(){
+    //     let output = '';
+    //     for (let i = 0; i < enemies2.length; i++) {
+    //         output += "<div class='enemy2' style='top:"+enemies2[i].y+"px; left:"+enemies2[i].x+"px;'></div>";
+    //     }
+    //     document.getElementById('enemies2').innerHTML = output; 
+    // }
 
     //make enemy moves
-    function moveBullets(){
-        for (let i = 0; i < bullets.length; i++) {
-            bullets[i].y -= 5;
-            //remove enemy when out of bounce.
-            if(bullets[i].y < 0){
-                bullets[i]  = bullets[bullets.length-1];
-                bullets.pop();
-            }
+    // function moveBullets(){
+    //     for (let i = 0; i < bullets.length; i++) {
+    //         bullets[i].y -= 5;
+    //         //remove enemy when out of bounce.
+    //         if(bullets[i].y < 0){
+    //             bullets[i]  = bullets[bullets.length-1];
+    //             bullets.pop();
+    //         }
             
-        }
+    //     }
         
-    }
+    // }
 
-    function moveEnemies(){
-        for (let i = 0; i < enemies.length; i++) {
-            enemies[i].y += 5;
-            //remove enemy when out of bounce.
-            if(enemies[i].y > 540){
-                enemies[i].y = 0;
-                enemies[i].x = Math.random()*500;
-            }
-        }
-    }
+    // function moveEnemies(){
+    //     for (let i = 0; i < enemies.length; i++) {
+    //         enemies[i].y += 5;
+    //         //remove enemy when out of bounce.
+    //         if(enemies[i].y > 540){
+    //             enemies[i].y = 0;
+    //             enemies[i].x = Math.random()*500;
+    //         }
+    //     }
+    // }
 
-    function moveEnemies2(){
-        for (let i = 0; i < enemies2.length; i++) {
-            enemies2[i].y += 5;
-            //remove enemy when out of bounce.
-            if(enemies2[i].y > 540){
-                enemies2[i].y = 0;
-                enemies2[i].x = Math.random()*500;
-            }
-        }
-    }
+    // function moveEnemies2(){
+    //     for (let i = 0; i < enemies2.length; i++) {
+    //         enemies2[i].y += 5;
+    //         //remove enemy when out of bounce.
+    //         if(enemies2[i].y > 540){
+    //             enemies2[i].y = 0;
+    //             enemies2[i].x = Math.random()*500;
+    //         }
+    //     }
+    // }
 
-    function displayBullets(){
-        let output = '';
-        for (let i = 0; i < bullets.length; i++) {
-            output += "<div class='bullet' style='top:"+bullets[i].y+"px; left:"+bullets[i].x+"px;'></div>";
-        }
-        document.getElementById('bullets').innerHTML = output;
-    }
+    // function displayBullets(){
+    //     let output = '';
+    //     for (let i = 0; i < bullets.length; i++) {
+    //         output += "<div class='bullet' style='top:"+bullets[i].y+"px; left:"+bullets[i].x+"px;'></div>";
+    //     }
+    //     document.getElementById('bullets').innerHTML = output;
+    // }
 
-    function displayScore() {
-        if(score < 0){
-            clearInterval(game);
-            document.getElementById('hero').style['display'] = "none";
-            document.getElementById('enemies').innerHTML = "";
-            document.getElementById('enemies2').innerHTML = "";
-            document.getElementById('bullets').innerHTML = "";
-            document.getElementById('gameover').style['display'] = "block";
-            document.getElementById('score').innerHTML = 0;
-        }else{
-            document.getElementById('score').innerHTML = score;	
-        }
+    // function displayScore() {
+    //     if(score < 0){
+    //         clearInterval(game);
+    //         document.getElementById('hero').style['display'] = "none";
+    //         document.getElementById('enemies').innerHTML = "";
+    //         document.getElementById('enemies2').innerHTML = "";
+    //         document.getElementById('bullets').innerHTML = "";
+    //         document.getElementById('gameover').style['display'] = "block";
+    //         document.getElementById('score').innerHTML = 0;
+    //     }else{
+    //         document.getElementById('score').innerHTML = score;	
+    //     }
         
-    }
+    // }
 
-    function detectCollision(){
-        for (let i = 0; i < bullets.length; i++) {
-            for (let j = 0; j < enemies.length; j++) {
-                if(Math.abs(bullets[i].x-5 - enemies[j].x-5) < 10 && Math.abs(bullets[i].y - enemies[j].y) < 10){
-                    mySound.play(); 
-                    score += 10;
-                    document.getElementById('enemies').innerHTML = "<div class='explode' style='top:"+enemies[j].y+"px; left:"+enemies[j].x+"px;'></div>";
-                    enemies[j].y = 0;
-                    bullets[i]  = bullets[bullets.length-1];
-                    bullets.pop();	 
-                }
-            }
+    // function detectCollision(){
+    //     for (let i = 0; i < bullets.length; i++) {
+    //         for (let j = 0; j < enemies.length; j++) {
+    //             if(Math.abs(bullets[i].x-5 - enemies[j].x-5) < 10 && Math.abs(bullets[i].y - enemies[j].y) < 10){
+    //                 mySound.play(); 
+    //                 score += 10;
+    //                 document.getElementById('enemies').innerHTML = "<div class='explode' style='top:"+enemies[j].y+"px; left:"+enemies[j].x+"px;'></div>";
+    //                 enemies[j].y = 0;
+    //                 bullets[i]  = bullets[bullets.length-1];
+    //                 bullets.pop();	 
+    //             }
+    //         }
             
-        }
-    }
+    //     }
+    // }
 
-    function detectCollision2(){
-        for (let i = 0; i < bullets.length; i++) {
-            for (let j = 0; j < enemies2.length; j++) {
-                if(Math.abs(bullets[i].x-5 - enemies2[j].x-5) < 10 && Math.abs(bullets[i].y - enemies2[j].y) < 10){
-                    mySound.play(); 
-                    score += 10;
-                    document.getElementById('enemies2').innerHTML = "<div class='explode' style='top:"+enemies2[j].y+"px; left:"+enemies2[j].x+"px;'></div>";
-                    enemies2[j].y = 0;
-                    bullets[i]  = bullets[bullets.length-1];
-                    bullets.pop();	 
-                }
-            }
+    // function detectCollision2(){
+    //     for (let i = 0; i < bullets.length; i++) {
+    //         for (let j = 0; j < enemies2.length; j++) {
+    //             if(Math.abs(bullets[i].x-5 - enemies2[j].x-5) < 10 && Math.abs(bullets[i].y - enemies2[j].y) < 10){
+    //                 mySound.play(); 
+    //                 score += 10;
+    //                 document.getElementById('enemies2').innerHTML = "<div class='explode' style='top:"+enemies2[j].y+"px; left:"+enemies2[j].x+"px;'></div>";
+    //                 enemies2[j].y = 0;
+    //                 bullets[i]  = bullets[bullets.length-1];
+    //                 bullets.pop();	 
+    //             }
+    //         }
             
-        }
-    }
+    //     }
+    // }
 
-    function detectCollisionEnemy(){
-        for (let i = 0; i < enemies.length; i++) {
-            if(Math.abs(hero.x - enemies[i].x-5) < 10 && Math.abs(hero.y - enemies[i].y) < 10){
-                enemies[i].y = 0;
-                document.getElementById('hero').style['display'] = "none";
-                setTimeout(function() {
-                    document.getElementById('hero').style['display'] = "block";
-                }, 50);
-                crash.play();
-                score -= 500; 
-            }
-        }	
-    }
+    // function detectCollisionEnemy(){
+    //     for (let i = 0; i < enemies.length; i++) {
+    //         if(Math.abs(hero.x - enemies[i].x-5) < 10 && Math.abs(hero.y - enemies[i].y) < 10){
+    //             enemies[i].y = 0;
+    //             document.getElementById('hero').style['display'] = "none";
+    //             setTimeout(function() {
+    //                 document.getElementById('hero').style['display'] = "block";
+    //             }, 50);
+    //             crash.play();
+    //             score -= 500; 
+    //         }
+    //     }	
+    // }
 
-    function detectCollisionEnemy2(){
-        for (let i = 0; i < enemies2.length; i++) {
-            if(Math.abs(hero.x - enemies2[i].x-5) < 10 && Math.abs(hero.y - enemies2[i].y) < 10){
-                enemies2[i].y = 0;
-                document.getElementById('hero').style['display'] = "none";
-                setTimeout(function() {
-                    document.getElementById('hero').style['display'] = "block";
-                }, 50);
-                crash.play();
-                score -= 500; 
-            }
-        }	
-    }
+    // function detectCollisionEnemy2(){
+    //     for (let i = 0; i < enemies2.length; i++) {
+    //         if(Math.abs(hero.x - enemies2[i].x-5) < 10 && Math.abs(hero.y - enemies2[i].y) < 10){
+    //             enemies2[i].y = 0;
+    //             document.getElementById('hero').style['display'] = "none";
+    //             setTimeout(function() {
+    //                 document.getElementById('hero').style['display'] = "block";
+    //             }, 50);
+    //             crash.play();
+    //             score -= 500; 
+    //         }
+    //     }	
+    // }
 
     function gameLoop(){
         displayHero();
--- script_OOP.js
+++ script_OOP.js
@@ -1,35 +1,139 @@
 window.onload = function() {
-    const mySound = new Audio('explosion effect.wav');
+    const explode = new Audio('explosion effect.wav');
     const crash = new Audio('crash effect.wav');
+    
+    class Characters{
+        displayHero(hero) {
+            document.getElementById('hero').style['top'] = hero.y + "px";
+            document.getElementById('hero').style['left'] = hero.x + "px";
+        }
+
+        displayEnemiesOrBullets(character_type, character_class, character_id){
+            let output = '';
+            for (let i = 0; i < character_type.length; i++) {
+                output += "<div class='"+character_class+"' style='top:"+character_type[i].y+"px; left:"+character_type[i].x+"px;'></div>";
+            }
+            document.getElementById(character_id).innerHTML = output;
+        }
+
+        moveEnemiesOrBullets(character_type, character_name){
+            for (let i = 0; i < character_type.length; i++) {
+                if(character_name == "bullet"){
+                    character_type[i].y -= 5;
+                    //remove enemy when out of bounce.
+                    if(character_type[i].y < 0){
+                        character_type[i]  = character_type[character_type.length-1];
+                        character_type.pop();
+                    }
+                }else if(character_name == "enemy"){
+                    character_type[i].y += 5;
+                    if(character_type[i].y > 540){
+                        character_type[i].y = 0;
+                        character_type[i].x = Math.random()*500;
+                    }
+                }
+            }
+        }
+
+    }
+
+    class Game_Detections{
+
+        constructor(score, explode, crash) {
+            this.score = score;
+            this.explode = explode;
+            this.crash = crash;
+        }
+
+        detectCollision(bullets, enemy_type, enemy_id){
+            for (let i = 0; i < bullets.length; i++) {
+                for (let j = 0; j < enemy_type.length; j++) {
+                    if(Math.abs(bullets[i].x-5 - enemy_type[j].x-5) < 10 && Math.abs(bullets[i].y - enemy_type[j].y) < 10){
+                        this.explode.play(); 
+                        this.score += 10;
+                        document.getElementById(enemy_id).innerHTML = "<div class='explode' style='top:"+enemy_type[j].y+"px; left:"+enemy_type[j].x+"px;'></div>";
+                        enemy_type[j].y = 0;
+                        bullets[i]  = bullets[bullets.length-1];
+                        bullets.pop();	 
+                    }
+                }   
+            }
+        }
+
+        detectCollisionEnemies(enemy_type, hero){
+            for (let i = 0; i < enemy_type.length; i++) {
+                if(Math.abs(hero.x - enemy_type[i].x-5) < 10 && Math.abs(hero.y - enemy_type[i].y) < 10){
+                    enemy_type[i].y = 0;
+                    document.getElementById('hero').style['display'] = "none";
+                    setTimeout(function() {
+                        document.getElementById('hero').style['display'] = "block";
+                    }, 50);
+                    this.crash.play();
+                    this.score -= 500; 
+                }
+            }	
+        }
+
+        displayScore(game) {
+            if(this.score < 0){
+                clearInterval(game);
+                is_gameOver = false;
+                document.getElementById('hero').style['display'] = "none";
+                document.getElementById('enemies').innerHTML = "";
+                document.getElementById('enemies2').innerHTML = "";
+                document.getElementById('bullets').innerHTML = "";
+                document.getElementById('gameover').style['display'] = "block";
+                document.getElementById('score').innerHTML = 0;
+            }else{
+                document.getElementById('score').innerHTML = this.score;	
+            }
+        }
+
+    }
+// ==================================================================================================================
     let score = 0;
+    let is_gameOver = true;
     const hero = {
         x: 300,
         y: 500
     }
 
-    const enemies = [{x:50,y:50}, {x:250, y:50}, {x:50,y:250}, {x:520,y:220}];
-    const enemies2 = [{x:300,y:100}, {x:380, y:20}, {x:100,y:50}];
+    const enemies = [{x:50,y:50}, {x:250, y:50}, {x:150,y:140}, {x:420,y:120}];
+    const enemies2 = [{x:550,y:70}, {x:350, y:20}, {x:100,y:50}];
     let bullets  = [];
+    let character = new Characters();
+    let game_detection = new Game_Detections(score, explode, crash);
     
-    class Character{
-        constructor(character) {
-            this.character = character;
-        }
-
-        displayHero() {
-            document.getElementById('hero').style['top'] = this.character.y + "px";
-            document.getElementById('hero').style['left'] = this.character.x + "px";
-        }
-
-        displayEnemies(){
-            let output = '';
-            for (let i = 0; i < this.character.length; i++) {
-                output += "<div class='enemy2' style='top:"+this.character[i].y+"px; left:"+this.character[i].x+"px;'></div>";
-            }
-            document.getElementById(this.character).innerHTML = output;
-        }
+    function gameLoop(){
+        character.displayHero(hero); //hero
+        character.displayEnemiesOrBullets(enemies,"enemy1","enemies"); //Enemy 1
+        character.displayEnemiesOrBullets(enemies2,"enemy2","enemies2"); //Enemy 2
+        character.displayEnemiesOrBullets(bullets,"bullet","bullets"); // Bullet
+        character.moveEnemiesOrBullets(enemies,"enemy");
+        character.moveEnemiesOrBullets(enemies2,"enemy");
+        character.moveEnemiesOrBullets(bullets,"bullet");
+        game_detection.detectCollision(bullets, enemies, "enemies");
+        game_detection.detectCollision(bullets, enemies2, "enemies2");
+        game_detection.detectCollisionEnemies(enemies, hero);
+        game_detection.detectCollisionEnemies(enemies2, hero);
+        game_detection.displayScore(game);   
     }
 
-    let my_hero = new Character(hero);
-    my_hero.displayEnemies();
+    const game = setInterval(gameLoop, 50);
+
+    document.onkeydown = function(key){
+        if(key.keyCode == 37 && is_gameOver && hero.x > 0){
+            hero.x -= 10;
+        }else if(key.keyCode == 39 && is_gameOver){
+            hero.x += 10;
+        }else if(key.keyCode == 40 && is_gameOver && hero.y < 530){ // down
+            hero.y += 10;
+        }else if(key.keyCode == 38 && is_gameOver && hero.y > 10){ //up
+            hero.y -= 10;
+        }else if(key.keyCode == 32 && is_gameOver){ //space
+            bullets.push({x: hero.x+8, y: hero.y-15});
+            character.displayEnemiesOrBullets(bullets,"bullet","bullets");
+        }
+        character.displayHero(hero);
+    }
 }
